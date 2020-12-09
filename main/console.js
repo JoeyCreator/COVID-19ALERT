ad4f82034188b31640fe902133b5daa9aad3e06a
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-12-09 15:23:54 -0500

    Try to add dark mode.

--- main/console.js
+++ main/console.js
@@ -12,3 +12,29 @@ console.log(
   "%cSee https://softwareqc.netlify.app for more information.",
   "color:black;font-family:system-ui;font-size:1.2rem;-webkit-text-stroke: 1px black;font-weight:arial"
 );
+
+function applyTheme(theme) {
+  document.body.classList.remove("theme-auto", "theme-light", "theme-dark");
+  document.body.classList.add(`theme-${theme}`);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#theme").addEventListener("change", function () {
+    applyTheme(this.value);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme") || "auto";
+
+  applyTheme(savedTheme);
+
+  for (const optionElement of document.querySelectorAll("#theme option")) {
+    optionElement.selected = savedTheme === optionElement.value;
+  }
+
+  document.querySelector("#theme").addEventListener("change", function () {
+    localStorage.setItem("theme", this.value);
+    applyTheme(this.value);
+  });
+});
