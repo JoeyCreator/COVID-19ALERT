87c7509a061f2c1db0dd09f9009221bcff8ca363
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-12-10 11:39:58 -0500

    Update 2 JavaScript File

--- pages/console.js
+++
@@ -1,9 +0,0 @@
-console.log(
-  "%cStop!",
-  "color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold"
-);
-
-console.log(
-  "%cThis is a browser feature intended for developers.",
-  "color:black;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:arial"
-);
