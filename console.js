a77ca35318b3bda5a7e42aa054578da47117ed2f
Joey Cadieux <58401407+JoeyCreator@users.noreply.github.com>
2020-12-13 11:18:04 -0500

    modify things

--- console.js
+++ console.js
@@ -12,3 +12,18 @@ console.log(
   "%cSee https://github.com/JoeyCreator/COVID-19ALERT/blob/main/LICENSE.md for more information.",
   "color:black;font-family:system-ui;font-size:1.2rem;-webkit-text-stroke: 1px black;font-weight:arial"
 );
+
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
