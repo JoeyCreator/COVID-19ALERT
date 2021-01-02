15b55d134db3fdc891b9cffafbd37b74ecd57ae3
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-01-02 22:49:45 +0000

    update site!!

--- console.js
+++ console.js
@@ -1,14 +1,2 @@
-
-var boolean = document.hidden
-
-
-
-console.log(
-  "%cStop!",
-  "color:red;font-family:system-ui;font-size:4rem;-webkit-text-stroke: 1px black;font-weight:bold"
-);
-
-console.log(
-  "%cUsing this console may allow attackers to impersonate you and steal your information using an attack called Self-XSS.Do not enter or paste code that you don't understand.",
-  "color:black;font-family:system-ui;font-size:1.54rem;-webkit-text-stroke: 1px black;font-weight:arial"
-);
+var divOne = document.getElementById('1');
+divOne.style.display='none';
