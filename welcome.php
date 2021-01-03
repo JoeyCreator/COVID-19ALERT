23745203b06d6fde12cda95e7516f43b66128084
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-01-03 17:46:44 +0000

    welcome.php

--- welcome.php
+++ welcome.php
@@ -1,17 +1,34 @@
 <?php
 
-$host='Localhost';
-$user='root';
-$pass='';
-$db='techwit2_auth';
+$host="localhost";
+$user="root";
+$password="";
+$db="techwit2_auth";
+
+mysql_connect($host,$user,$password);
+$mysql_select_db($db);
+
+if(isset(['username'])) {
+  $username=$_POST['username'];
+  $password=$_POST['password'];
 
 
-$con=mysqli_connect($host,$user,$pass,$db);
-if($con)
-  echo 'connected successfully to auth database';
+ $sql="select * from techwit2_auth where username='".$username."'"AND password'".$password."limit 1";
 
-  $sql="insert into techwit2_auth (username,password,email)"
-  $query=mysqli_query($con,$sql);
-  if($query)
-    echo 'echo inserted successfully';
-    ?>
+ $result=mysql_query($sql);
+
+ if(mysql_num_rows($result)==1){
+    echo " you have successfully logged in ";
+    exit();
+ }
+ else{
+   echo " you have entered inccorect password ";
+   exit();
+ }
+
+
+}
+
+
+
+?>
